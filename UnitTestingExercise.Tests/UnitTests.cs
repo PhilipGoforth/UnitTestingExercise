85ceb21892d6d8a28ace5312acd1841069b4a2b1
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-11-17 14:28:28 -0600

    final

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,81 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 1, 1, 3)]
+        [InlineData(2, 2, 2, 6)]
+        [InlineData(3, 3, 3, 9)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            int actual = test.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(5, 3, 2)]
+        [InlineData(3, 3, 0)]
         public void Subtract(int minuend, int subtrhend, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            int actual = test.Subtract(minuend, subtrhend);
             //Assert
+            Assert.Equal(expected, actual);
 
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2, 2, 4)]
+        [InlineData(10, 10, 100)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            int actual = test.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10, 2, 5)]
+        [InlineData(50, 10, 5)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            var test = new UnitTestMethods();
             //Act
-
+            int actual = test.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
         [Fact]
-        public void YourMethodName()
+        public void IsANumber()
         {
             //Arrange
-
+            var test = new UnitTestMethods();
+            var str = "twelve";
             //Act
-
+            bool result = test.IsANumber(str);
             //Assert
-
+            Assert.False(result, $"{str} is not a number.");
         }
 
         [Fact]
-        public void YourMethodName2()
+        public void IsPrime()
         {
             //Arrange
+            var test = new UnitTestMethods();
 
             //Act
-
+            var actual = test.SayHi();
             //Assert
+            Assert.Equal("Hi", actual);
         }
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 namespace UnitTestingExercise
 {
     public class UnitTestMethods
@@ -9,18 +11,36 @@ namespace UnitTestingExercise
 
 
         // Create an Add method that passes 3 integers
-
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
-
+        // Keep track of which number is getting passed as minuend and subtrahend
+        public int Subtract(int minuend, int subtrhend)
+        {
+            return minuend - subtrhend;
+        }
         // Create a Multiply method that passes 2 integers
-
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
         // Create a Divide method that passes 2 integers
 
-
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
-
-        
+        public bool IsANumber(string str)
+        {
+            return str.All(char.IsNumber);
+        }
+        public string SayHi()
+        {
+            return "Hi";
+        }
     }
 }
